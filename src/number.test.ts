43888fe1bd1a82aff0a098113025f324afc72f51
mountainpath <mountainpath9@protonmail.com>
2025-05-29 18:02:48 +1000

    Replace fromNumber() with a version that keeps full precision

---
+++ src/number.test.ts
@@ -0,0 +1,59 @@
+import { expect, test } from 'vitest'
+import { getNumberParts } from './number';
+
+test('parts', () => {
+  // not exactly comprehensive
+
+  {
+    const parts = getNumberParts(0);
+    expect(parts.sign).toBe(0);
+    expect(parts.mantissa).toBe(0);
+    expect(parts.biasedExponent).toBe(-1023);
+  }
+
+  {
+    const parts = getNumberParts(1);
+    expect(parts.sign).toBe(0);
+    expect(parts.mantissa).toBe(0);
+    expect(parts.biasedExponent).toBe(0);
+  }
+
+  {
+    const parts = getNumberParts(-1);
+    expect(parts.sign).toBe(1);
+    expect(parts.mantissa).toBe(0);
+    expect(parts.biasedExponent).toBe(0);
+  }
+
+  {
+    const parts = getNumberParts(1.5);
+    expect(parts.sign).toBe(0);
+    expect(parts.mantissa).toBe(MAX_MANTISSA / 2);
+    expect(parts.biasedExponent).toBe(0);
+  }
+
+
+  {
+    const parts = getNumberParts(2);
+    expect(parts.sign).toBe(0);
+    expect(parts.mantissa).toBe(0);
+    expect(parts.biasedExponent).toBe(1);
+  }
+
+  {
+    const parts = getNumberParts(3);
+    expect(parts.sign).toBe(0);
+    expect(parts.mantissa).toBe(MAX_MANTISSA / 2);
+    expect(parts.biasedExponent).toBe(1);
+  }
+
+  {
+    const parts = getNumberParts(0.5);
+    expect(parts.sign).toBe(0);
+    expect(parts.mantissa).toBe(0);
+    expect(parts.biasedExponent).toBe(-1);
+  }
+});
+
+export const MAX_MANTISSA = 2**52;
+export const MAX_MANTISSA_BI = BigInt(2**52);
